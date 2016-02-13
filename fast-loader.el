b69298a7adacdd75fa93126bd59ac35b131714c3
Yusuke Watanabe <yusukew052@gmail.com>
2016-02-14 07:24:49 +0900

    fix byte compile

--- fast-loader.el
+++ fast-loader.el
@@ -14,7 +14,7 @@
   :group 'fast-loader)
 
 (defun fast-loader-needs-update ()
-  "Returns t if need to update the cache file"
+  "Returns t if need to update the cache file."
   (let ((cache-modtime (nth 5 (file-attributes "~/.emacs.d/fast-loader-cache/inits.el")))
         (inits-modtime (nth 6(nth 0 (sort (directory-files-and-attributes "~/.emacs.d/inits")
                                           #'(lambda (x y) (time-less-p (nth 6 y) (nth 6 x))))))))
@@ -26,16 +26,15 @@
   (if (fast-loader-needs-update)
       (progn
         (let* ((elfile (expand-file-name (concat fast-loader-cache-dir "/" fast-loader-cache-filename)))
-               (cmd (concat "cd " inits-dir " && cat * | sed -e 's/;;.*$//' | sed -e '/^ *$/d' > " elfile)))
+               (cmd (concat "cd " inits-dir " && cat * | sed -e 's/;;.*$//' | sed -e '/^ *$/d' > " elfile))
+               (elcfile (concat elfile "c"))
+               )
           ;; execute: cd inits-dir && cat * | sed -e 's/;;.*$//' | sed -e '/^ *$/d' > path/to/fast-loader-cache-dir/fast-loader-cache-filename
           (shell-command cmd)
           (when fast-loader-byte-compile
-            (add-hook 'after-init-hook
-                      '(lambda ()
-                         (message elfile)
-                         (let* ((elcfile (concat elfile "c")))
-                           (when (file-newer-than-file-p elfile elcfile)
-                             (byte-compile-file el)))))
+            (if (file-exists-p elcfile)
+                (byte-recompile-file elfile)
+              (byte-compile-file elfile))
             ))
         ))
   (add-to-list 'load-path fast-loader-cache-dir)
