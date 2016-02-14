72ff4638f13c0ded33f7c14c3f8a34a75abaafff
Yusuke Watanabe <yusukew052@gmail.com>
2016-02-14 17:17:23 +0900

    fix cache update logic

--- fast-loader.el
+++ fast-loader.el
@@ -13,30 +13,33 @@
   :type 'boolean
   :group 'fast-loader)
 
-(defun fast-loader-needs-update ()
+(defun fast-loader-needs-update (inits cache)
   "Returns t if need to update the cache file."
-  (let ((cache-modtime (nth 5 (file-attributes "~/.emacs.d/fast-loader-cache/inits.el")))
-        (inits-modtime (nth 6(nth 0 (sort (directory-files-and-attributes "~/.emacs.d/inits")
-                                          #'(lambda (x y) (time-less-p (nth 6 y) (nth 6 x))))))))
-    (time-less-p cache-modtime inits-modtime))
-  )
+  (if (file-exists-p cache)
+      (progn
+        (let ((cache-modtime (nth 5 (file-attributes cache)))
+              (sorted (sort (directory-files-and-attributes inits)
+                            #'(lambda (x y) (time-less-p (nth 6 y) (nth 6 x))))))
+          (if (string-equal (car (car sorted)) "..")
+              (time-less-p cache-modtime (nth 6 (nth 1 sorted)))
+            (time-less-p cache-modtime (nth 6 (car sorted))))))
+    t))
 
 (defun fast-loader-load (inits-dir)
   "Load init files in INITS-DIR."
-  (if (fast-loader-needs-update)
-      (progn
-        (let* ((elfile (expand-file-name (concat fast-loader-cache-dir "/" fast-loader-cache-filename)))
-               (cmd (concat "cd " inits-dir " && cat * | sed -e 's/;;.*$//' | sed -e '/^ *$/d' > " elfile))
-               (elcfile (concat elfile "c"))
-               )
-          ;; execute: cd inits-dir && cat * | sed -e 's/;;.*$//' | sed -e '/^ *$/d' > path/to/fast-loader-cache-dir/fast-loader-cache-filename
-          (shell-command cmd)
-          (when fast-loader-byte-compile
-            (if (file-exists-p elcfile)
-                (byte-recompile-file elfile)
-              (byte-compile-file elfile))
-            ))
-        ))
+  (let ((elfile (expand-file-name (concat fast-loader-cache-dir "/" fast-loader-cache-filename))))
+    (if (fast-loader-needs-update inits-dir elfile)
+        (progn
+          (if (not (file-exists-p fast-loader-cache-dir))
+              (make-directory fast-loader-cache-dir))
+          (let ((cmd (concat "cd " inits-dir " && cat *.el | sed -e 's/;;.*$//' | sed -e '/^ *$/d' > " elfile))
+                (elcfile (concat elfile "c")))
+            ;; execute: cd inits-dir && cat *.el | sed -e 's/;;.*$//' | sed -e '/^ *$/d' > path/to/fast-loader-cache-dir/fast-loader-cache-filename
+            (shell-command cmd)
+            (when fast-loader-byte-compile
+              (if (file-exists-p elcfile)
+                  (byte-recompile-file elfile)
+                (byte-compile-file elfile)))))))
   (add-to-list 'load-path fast-loader-cache-dir)
   (load (file-name-sans-extension fast-loader-cache-filename)))
 
